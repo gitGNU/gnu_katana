5c797a8975f20ccf456b020cbc9f04fca4171833
James Oakley <james@electron.kiewit.dartmouth.edu>
2010-02-18 20:07:59 -0500

    added functions for manipulating paths

---
+++ katana/src/util/path.h
@@ -0,0 +1,14 @@
+/*
+  File: path.h
+  Author: James Oakley
+  Project:  katana
+  Date: February 2010
+  Description: Functions for dealing with filepaths
+*/
+
+#ifndef path_h
+#define path_h
+
+char* joinPaths(char* path1,char* path2);
+
+#endif
