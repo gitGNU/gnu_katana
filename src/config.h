a54753dcf46badb36c6fd7ecabe54763ab77e530
James Oakley <james@electron.kiewit.dartmouth.edu>
2010-02-23 22:04:30 -0500

    forgot new files

---
+++ src/config.h
@@ -0,0 +1,22 @@
+/*
+  File: config.h
+  Author: James Oakley
+  Project:  katana
+  Date: February 2010
+  Description: Program environment, options that can be configured
+*/
+
+#ifndef config_h
+#define config_h
+#include <stdbool.h>
+
+typedef enum
+{
+  EKCF_CHECK_PTRACE_WRITES,//check writes done into target memory
+  EKCF_COUNT
+} E_KATANA_CONFIG_FLAGS;
+void setDefaultConfig();//called on startup
+bool isFlag(E_KATANA_CONFIG_FLAGS flag);
+void setFlag(E_KATANA_CONFIG_FLAGS flag,bool state);
+
+#endif
