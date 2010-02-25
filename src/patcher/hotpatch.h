a3ea27b71c4d49baf23b14b61304c9fc19e605c5
James Oakley <james@electron.kiewit.dartmouth.edu>
2010-02-24 21:41:57 -0500

    moved a legacy function to katana prelim

--- src/patcher/hotpatch.h
+++ src/patcher/hotpatch.h
@@ -9,8 +9,6 @@
 #ifndef hotpatch_h
 #define hotpatch_h
 #include "types.h"
-//!!! legacy deprecated function, don't use it
-bool fixupVariable(VarInfo var,TransformationInfo* trans,int pid,ElfInfo* e);
 addr_t getFreeSpaceForTransformation(TransformationInfo* trans,uint howMuch);
 void performRelocations(ElfInfo* e,VarInfo* var);
 #endif
