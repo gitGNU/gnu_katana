3122cedf5de289e6371963386c6cc57209b3f463
James Oakley <james.oakley@dartmouth.edu>
2010-03-01 12:05:21 -0500

    added size_t map constructor to map and appropriate hash functions

--- katana/src/util/hash.h
+++ katana/src/util/hash.h
@@ -7,7 +7,20 @@
 #ifndef _HASH_H__
 #define _HASH_H__
 
+#include <stdint.h> //for uint32_t and uint64_t
+
+
+#if __WORDSIZE==64
+//#define hashSizeT(a) hash64Bit(a)
+#define hashSizeT    hash64Bit
+#else
+//#define hashSizeT(a) hash32Bit(a)
+#define hashSizeT    hash32Bit
+#endif
+
 unsigned long hash1(char*);
 
 unsigned long hashInt(int);
+uint32_t hash32Bit(uint32_t key);
+uint64_t hash64Bit(uint64_t key);
 #endif
