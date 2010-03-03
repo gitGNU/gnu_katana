285364d098e7d8ea5abe4d14ebac7cd1626f078a
James Oakley <james.oakley@dartmouth.edu>
2010-03-02 20:34:51 -0500

    test5 finally works

--- tests/t5_pointers/validate.py
+++ tests/t5_pointers/validate.py
@@ -8,7 +8,7 @@ def validate(logname):
 
   regexAlphaLives="alpha lives at 0x([a-f0-9]*)"
   regexAlphaVal="alpha: 1, 2, 3"
-  regexAlphaOtherLives="alpha\.other lives at (0x[a-f0-9]*)"
+  regexAlphaOtherLives="alpha\.other lives at 0x([a-f0-9]*)"
   regexAlphaOtherVal="alpha\.other: 33, 22, 11"
   regexAlphaOtherOther="alpha\.other has no other member"
   regexBetaLives="beta lives at 0x([a-f0-9]*)"
@@ -25,20 +25,29 @@ def validate(logname):
 
   
   linecount=0
-  patchingHappened=[False,False,False] #one each for alpha, beta and gamma
-  addrs=[None,None,None] #one entry each for alpha, beta, and gamma
+  addressChanged={}
+  addressChanged['alpha']=False
+  addressChanged['beta']=False
+  addressChanged['gamma']=False
+  addrs={}
+  addrs['alpha']=None
+  addrs['beta']=None
+  addrs['gamma']=None
 
   def validateAddr(var,match):
     if not addrs[var]:
       addrs[var]=match.group(1)
       return True
     if addrs[var]!=match.group(1):
-      if not patchingHappened[var]:
-        patchingHappened[var]=True
+      if not addressChanged[var]:
+        addressChanged[var]=True
         addrs[var]=match.group(1)
       else:
         sys.stderr.write("Address for var "+var+" changed more than once\n");
+        sys.stderr.write("Previous address was 0x"+addrs[var]+"\n")
+        sys.stderr.write("And the new one is 0x"+match.group(1)+"\n");
         return False
+    return True
     
 
   for line in f:
@@ -52,7 +61,7 @@ def validate(logname):
       sys.stderr.write("and the pattern (regex) is:\n")
       sys.stderr.write(pattern)
       sys.stderr.write("\n")
-      if patchingHappened:
+      if addressChanged['alpha']:
         sys.stderr.write("Note, patching has been done\n")
     def validationError():
       sys.stderr.write("Line "+str(linecount)+" did not pass validation\n")
@@ -68,26 +77,31 @@ def validate(logname):
       pattern=regexName
     elif line.startswith("alpha lives"):
       pattern=regexAlphaLives
+      validateFunc=lambda m: validateAddr('alpha',m)
     elif line.startswith("alpha:"):
       pattern=regexAlphaVal
     elif line.startswith("alpha.other lives"):
       pattern=regexAlphaOtherLives
+      validateFunc=lambda m: validateAddr('beta',m)
     elif line.startswith("alpha.other:"):
       pattern=regexAlphaOtherVal
     elif line.startswith("alpha.other has"):
       pattern=regexAlphaOtherOther
     elif line.startswith("beta lives"):
       pattern=regexBetaLives
+      validateFunc=lambda m: validateAddr('beta',m)
     elif line.startswith("beta:"):
       pattern=regexBetaVal
     elif line.startswith("beta has"):
       pattern=regexBetaOther
     elif line.startswith("gamma lives"):
       pattern=regexGammaLives
+      validateFunc=lambda m: validateAddr('gamma',m)
     elif line.startswith("gamma:"):
       pattern=regexGammaVal
     elif line.startswith("gamma.other lives"):
       pattern=regexGammaOtherLives
+      validateFunc=lambda m: validateAddr('alpha',m)
     elif line.startswith("gamma.other:"):
       pattern=regexGammaOtherVal
     elif line.startswith("gamma.other.other lives"):
@@ -107,13 +121,13 @@ def validate(logname):
       if not validateFunc(match):
         validationError()
         return False
-  if not patchingHappened[0]:
+  if not addressChanged['alpha']:
     sys.stderr.write("It appears patching of alpha never happened\n")
     return False
-  if not patchingHappened[1]:
+  if not addressChanged['beta']:
     sys.stderr.write("It appears patching of beta never happened\n")
     return False
-  if not patchingHappened[2]:
+  if not addressChanged['gamma']:
     sys.stderr.write("It appears patching of gamma never happened\n")
     return False
   return True
