042abc8065b4d3280294d6ad72e12ee583b2c52a
James Oakley <james.oakley@dartmouth.edu>
2010-03-11 16:47:11 -0500

    updated run tests to be able to start running testing of apache. I have not committed all of the apache stuff because it would be too large

--- katana/run_tests.py
+++ katana/run_tests.py
@@ -16,12 +16,12 @@ if os.path.exists("katana_err_log"):
 if os.path.exists("validator_log"):
   os.remove("validator_log")
 
-def runTestInDir(dir):
+def runTestInDir(dir,execName="test"):
   msg="running test "+os.path.basename(dir)
   dots=string.join(['.' for x in range(0,35-len(msg))],'')
   sys.stdout.write(msg+dots+'|')
   sys.stdout.flush()
-  if 0==subprocess.call(["python","validator.py",dir]):
+  if 0==subprocess.call(["python","validator.py",dir,execName]):
     sys.stdout.write("PASSED\n")
     return True
   else:
@@ -29,18 +29,26 @@ def runTestInDir(dir):
     return False
 
 if len(sys.argv)>1:
-  print "Taking tests to run from the command line"
-  for i in range(1,len(sys.argv)):
-    if True==runTestInDir(sys.argv[1]):
-      passTests+=1
-    totalTests+=1
+  print "Taking test to run from the command line"
+  if len(sys.argv)>2:
+    execName=sys.argv[2]
+  else:
+    execName="test"
+  if True==runTestInDir(sys.argv[1],execName):
+    passTests+=1
+  totalTests+=1
 else:
   print "Running all tests"
   for dir in sorted(os.listdir("tests")):
-    if os.path.isdir("tests/"+dir) and dir.startswith("t"):
-      if True==runTestInDir("tests/"+dir):
-        passTests+=1
-      totalTests+=1
+    if os.path.isdir("tests/"+dir):
+      if dir.startswith("t"):
+        if True==runTestInDir("tests/"+dir):
+          passTests+=1
+        totalTests+=1
+  #run the special tests, for large, real programs
+  if True==runTestInDir("real_tests/apache","httpd"):
+    passTests+=1
+  totalTests+=1
 
 print "Passed "+str(passTests)+" out of "+str(totalTests)+" tests"
 if passTests!=totalTests:
