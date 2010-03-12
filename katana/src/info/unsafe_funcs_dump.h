ace6ee8e69f467e4d966e864b7b7fcc31ad70e28
James Oakley <james.oakley@dartmouth.edu>
2010-03-12 16:10:27 -0500

    printing of unsafe functions

---
+++ katana/src/info/unsafe_funcs_dump.h
@@ -0,0 +1,15 @@
+/*
+  File: unsafe_funcs_dump.h
+  Author: James Oakley
+  Project:  katana
+  Date: March 2010
+  Description: Print a list of functions which are unsafe, patching
+               will not occur while they have activation fames on the stack
+*/
+
+#ifndef unsafe_funcs_dump_h
+#define unsafe_funcs_dump_h
+#include "elfparse.h"
+void printPatchUnsafeFuncsInfo(ElfInfo* patch);
+
+#endif
