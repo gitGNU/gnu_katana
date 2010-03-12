046c017256ffe7e3821db1675f65d97ea18c1cd3
James Oakley <james.oakley@dartmouth.edu>
2010-03-12 14:15:07 -0500

    added some more path functions

--- katana/src/util/path.c
+++ katana/src/util/path.c
@@ -9,8 +9,11 @@
 #include <string.h>
 #include <stdbool.h>
 #include "util.h"
-
-
+#include <sys/stat.h>
+#include "logging.h"
+#include "path.h"
+#include <limits.h>
+#include <unistd.h>
 
 char* joinPaths(char* path1,char* path2)
 {
@@ -34,4 +37,60 @@ char* joinPaths(char* path1,char* path2)
     strcat(result,path2);
   }
   return result;
+}
+
+//path is made relative to ref
+char* makePathRelativeTo(char* path,char* ref)
+{
+  char* path_=absPath(path);
+  char* ref_=absPath(ref);
+  if(!strncmp(path_,ref_,strlen(ref_)))
+  {
+    char* result=strdup(path_+strlen(ref_));
+    free(path_);
+    free(ref_);
+    return result;
+  }
+  else
+  {
+    death("Need to implement more compilated forms of makePathRelativeTo. Poke James\n");
+    return NULL;
+  }
+}
+
+//return the directory portion of path
+//the result should be freed
+char* getDirectoryOfPath(char* path)
+{
+  struct stat s;
+  if(0!=stat(path,&s))
+  {
+    logprintf(ELL_WARN,ELS_PATH,"path %s does not exist, cannot determine the directory portion\n",path);
+  }
+  if(S_ISDIR(s.st_mode))
+  {
+    return strdup(path);//the whole thing is the directory portion
+  }
+  else
+  {
+    char* lastSlash=strrchr(path,'/');
+    if(!lastSlash)
+    {
+      return strdup("");
+    }
+    char* result=strdup(path);
+    result[lastSlash-path+1]='\0';
+    return result;
+  }
+}
+
+//The returned string should be freed
+char* absPath(char* path)
+{
+  return realpath(path,NULL);
+}
+
+bool isAbsPath(char* path)
+{
+  return path[0]=='/';
 }
--- katana/src/util/path.h
+++ katana/src/util/path.h
@@ -8,7 +8,17 @@
 
 #ifndef path_h
 #define path_h
+#include <stdbool.h>
 
 char* joinPaths(char* path1,char* path2);
 
+//path is made relative to ref
+char* makePathRelativeTo(char* path,char* ref);
+
+//return the directory portion of path
+char* getDirectoryOfPath(char* path);
+
+//The returned string should be freed
+char* absPath(char* path);
+bool isAbsPath(char* path);
 #endif
