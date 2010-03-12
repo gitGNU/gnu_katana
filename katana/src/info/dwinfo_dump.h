0b018ec3ff9d549ab757d4da0ba061f6d0ea0622
James Oakley <james.oakley@dartmouth.edu>
2010-03-12 14:31:39 -0500

    support for listing much more information with the -l flag to katana

---
+++ katana/src/info/dwinfo_dump.h
@@ -0,0 +1,12 @@
+/*
+  File: dwinfo_dump.h
+  Author: James Oakley
+  Project:  katana
+  Date: March 2010
+  Description: Dumps patch information from the dwarf section of the patch
+*/
+
+#ifndef dwinfo_dump_h
+#define dwinfo_dump_h
+void printPatchDwarfInfo(ElfInfo* patch);
+#endif
