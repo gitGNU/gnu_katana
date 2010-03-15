eeb8528097adeeac80c66c8c1b12d9fef6323c7f
James Oakley <james.oakley@dartmouth.edu>
2010-03-15 14:34:04 -0400

    added test7 to unit test patching of consts

---
+++ tests/t7_const/v0/main.c
@@ -0,0 +1,47 @@
+/*
+  File: v0/main.c
+  Author: James Oakley
+  Project: Katana - Preliminary Work
+  Date: January 10
+  Description: Very simple program that exists to have one of its data
+               types patched. v1/main.c is the same thing with slight
+               changes to its data types
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MILLISECOND 1000
+
+typedef struct _Foo
+{
+  int field1;
+  int field2;
+  int field3;
+} Foo;
+
+
+const int alpha=2;
+const Foo beta={42,43,44};
+
+
+void printThings()
+{
+  printf("alpha: %i\n",alpha);
+  printf("beta: %i,%i,%i\n",beta.field1,beta.field2,beta.field3);
+  fflush(stdout);
+}
+
+int main(int argc,char** argv)
+{
+  printf("has pid %i\n",getpid());
+  while(1)
+  {
+    printThings();
+    usleep(100*MILLISECOND);
+  }
+  return 0;
+}
---
+++ tests/t7_const/validate.py
@@ -0,0 +1,93 @@
+import sys,re
+def validate(logname):
+  f=open(logname)
+  if not f:
+    sys.stderr.write("Cannot open log file for test3\n");
+    return False
+
+
+  regexAlpha="alpha: ([0-9]*)"
+  regexBeta="beta: ([0-9]*),([0-9]*),([0-9]*)"
+
+  
+  linecount=0
+  global alphaChanged,betaChanged #I don't get why this is necessary,
+                                  #should be closure, I don't
+                                  #understand python well enough
+  alphaChanged=False
+  betaChanged=False
+
+  def validateAlpha(match):
+    global alphaChanged #I don't get why this is necessary,
+                        #should be closure, I don't
+                        #understand python well enough
+    if alphaChanged==True:
+      if match.group(1)!="3":
+        sys.stderr.write("alpha should be 3, is "+match.group(1)+"\n");
+        return False
+    else:
+      if match.group(1)=="3":
+        alphaChanged=True
+      elif match.group(1)!="2":
+        sys.stderr.write("alpha should be 2, is "+match.group(1)+"\n");
+        return False
+    return True
+
+  def validateBeta(match):
+    global betaChanged
+    if betaChanged:
+      if match.group(1)!="45" and match.group(2)!="43" and match.group(3)!="46":
+        sys.stderr.write("beta should be 45,43,46, is "+match.group(1)+","+match.group(2)+","+match.group(3)+"\n");
+        return False
+    else:
+      if match.group(1)=="45":
+        betaChanged=True
+      if match.group(1)!="42" and match.group(2)!="43" and match.group(3)!="44":
+        sys.stderr.write("beta should be 42,43,44, is "+match.group(1)+","+match.group(2)+","+match.group(3)+"\n");
+        return False
+    return True
+
+
+  for line in f:
+    validateFunc=None
+    linecount+=1
+    patern="(no pattern chosen)"
+    def patternError():
+      sys.stderr.write("Line "+str(linecount)+" did not match  pattern\n")
+      sys.stderr.write("Offending line is:\n")
+      sys.stderr.write(line)
+      sys.stderr.write("and the pattern (regex) is:\n")
+      sys.stderr.write(pattern)
+      sys.stderr.write("\n")
+      if addressChanged['alpha']:
+        sys.stderr.write("Note, patching has been done\n")
+    def validationError():
+      sys.stderr.write("Line "+str(linecount)+" did not pass validation\n")
+      sys.stderr.write("Offending line is:\n")
+      sys.stderr.write(line)
+    if line.startswith("has pid"):
+      continue
+    elif line.startswith("alpha:"):
+      pattern=regexAlpha
+      validateFunc=validateAlpha
+    elif line.startswith("beta:"):
+      pattern=regexBeta
+      validateFunc=validateBeta
+    else:
+      pattern="line doesn't even start like any pattern"
+      patternError()
+    match=re.search(pattern,line)
+    if not match:
+      patternError()
+      return False
+    if validateFunc:
+      if not validateFunc(match):
+        validationError()
+        return False
+  if not betaChanged:
+    sys.stderr.write("It appears patching of beta never happened\n")
+    return False
+  if not alphaChanged:
+    sys.stderr.write("It appears patching of alpha never happened\n")
+    return False
+  return True
