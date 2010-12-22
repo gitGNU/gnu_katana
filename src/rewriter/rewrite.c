d7054820f537820ea368bd149c6d5df2b4a1096d
James Oakley <electron100@gmail.com>
2010-12-22 14:55:06 -0500

    added rewriter as a test, although it may be going away (repo still non-compiling)

---
+++ src/rewriter/rewrite.c
@@ -0,0 +1,24 @@
+/*
+  File: rewrite.c
+  Author: James Oakley
+  Copyright (C): 2010 James Oakley
+  License: Katana is free software: you may redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation, either version 2 of the
+  License, or (at your option) any later version.
+
+  This file was not written while under employment by Dartmouth
+  College and the attribution requirements on the rest of Katana do
+  not apply to code taken from this file.
+  Project:  katana
+  Date: November 2010
+  Description: Methods to rewrite a binary
+*/
+
+#include "elfparse.h"
+
+//at the moment this function merely reads in an object and rewrites it out
+void rewrite(ElfInfo* object,char* outfname)
+{
+  writeOutElf(object,outfname,true);
+}
