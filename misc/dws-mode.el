839bb612708d1ff2d90e6330a2dca5eea4465b97
James Oakley <electron100@gmail.com>
2011-05-06 16:22:43 -0400

    updated the emacs mode with some forgotten properties and a couple recently added properties

--- misc/dws-mode.el
+++ misc/dws-mode.el
@@ -11,7 +11,7 @@
   :group 'dws)
 
 (defvar dws-keywords
-  '("begin" "end")
+  '("begin" "end" "false")
   "Dwarfscript Keywords")
 
 (defvar dws-types
@@ -26,12 +26,14 @@
     "DW_EH_PE_absptr" "DW_EH_PE_uleb128" "DW_EH_PE_sleb128")
   "Dwarfscript Symbolic Constants")
 
+;;these are property names
 (defvar dws-variables
   '("section_type" "eh_hdr_addr" "section_addr" "except_table_addr" "index"
     "version" "data_align" "code_align" "return_addr_rule" "fde_ptr_enc" 
     "fde_lsda_ptr_enc" "personality_ptr_enc" "personality" "cie_index" 
     "initial_location" "address_range" "lsda_idx" "position" "length" 
-    "landing_pad" "has_action" "lpstart" "typeinfo_enc" "first_action")
+    "landing_pad" "has_action" "lpstart" "typeinfo_enc" "first_action"
+    "next" "typeinfo" "type_idx" "eh_hdr_table_enc")
   
   "Dwarfscript Properties")
 
@@ -42,17 +44,25 @@
     (list
      (cons (regexp-opt dws-keywords 'words) 'font-lock-keyword-face)
      (cons (regexp-opt dws-types 'words) 'font-lock-type-face)
+     ;;these are property names
      (cons (regexp-opt dws-variables 'words) 'font-lock-variable-name-face)
+     ;;symbolic constants
      (cons (regexp-opt dws-builtins 'words) 'font-lock-builtin-face)
+     ; DWARF instruction names
      (cons "\\(?:DW_CFA\\|DW_OP\\)_[a-zA-Z0-9_]*" 'font-lock-function-name-face)
-     (cons "[ \t]*[a-zA-Z_][a-zA-Z_0-9]*:" 'font-lock-preprocessor-face))))
+     ;identifiers. Not preprocessor, but it's a handy predefined face
+     ;todo: use something that makes more sense. Do I even need to highlight these?
+     ;(cons "[ \t]*[a-zA-Z_][a-zA-Z_0-9]*:" 'font-lock-preprocessor-face)
+     )))
 
 (define-derived-mode dws-mode fundamental-mode "DWS"
   "Major mode for editing Katana dwarfscript files"
   (setq font-lock-defaults '(dws-font-lock-keywords nil nil ((?_ . "w"))))
   (modify-syntax-entry ?# "< b" dws-mode-syntax-table)
   (modify-syntax-entry ?\n "> b" dws-mode-syntax-table)
-  (set (make-local-variable 'indent-line-function) 'dws-indent-line))
+  (set (make-local-variable 'indent-line-function) 'dws-indent-line)
+  (setq comment-start "# "))
+
 
 (defun dws-indent-line ()
   "Indent current line of dws code."
